bd2a3d000edd3df6da1a5f6fcfe992ff2c36d432
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2020-07-10 13:00:23 +0200

    adding binaries (bad style, but this is a sample project and won't change a lot)

---
+++ Binaries/BinarySampleHelloWorldObjC.xcframework/ios-x86_64-simulator/BinarySampleHelloWorldObjC.framework/Headers/Echo.h
@@ -0,0 +1,18 @@
+//
+//  Header.h
+//  
+//
+//  Created by Rainer Schlönvoigt on 10/07/2020.
+//
+
+@import Foundation;
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface EchoObjC : NSObject
+
+- (NSString*)shout:(NSString*)input;
+
+@end
+
+NS_ASSUME_NONNULL_END
