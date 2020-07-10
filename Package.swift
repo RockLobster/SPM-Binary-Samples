43fccb0f88365b0f80f6b4224d6134ac989338f2
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2020-07-10 12:59:44 +0200

    adding binary targets to swift.package

--- Package.swift
+++ Package.swift
@@ -5,26 +5,44 @@ import PackageDescription
 
 let package = Package(
     name: "SPM-Binary-Samples",
+    platforms: [
+        .iOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "SourceSampleHelloWorldSwift",
-            targets: ["SourceSampleHelloWorldSwift"]),
-
-        .library(
-            name: "SourceSampleHelloWorldObjC",
-            targets: ["SourceSampleHelloWorldObjC"]),
+            name: "SPM-Binary-Samples",
+            targets: [
+                "SourceSampleHelloWorldSwift",
+                "SourceSampleHelloWorldObjC",
+                "BinarySampleHelloWorldSwift",
+                "BinarySampleHelloWorldObjC"
+            ]
+        )
     ],
     dependencies: [],
     targets: [
 
-        // A Swift target build from Source code
+        // A Swift target built from source code
         .target(
             name: "SourceSampleHelloWorldSwift",
-            path: "Sources/Swift"),
+            path: "Sources/Swift",
+            exclude: [ "Info.plist" ]),
 
+        // An ObjC target built from source code
         .target(
             name: "SourceSampleHelloWorldObjC",
-            path: "Sources/ObjC")
+            path: "Sources/ObjC",
+            exclude: [ "Info.plist" ]),
+
+        // A bundled Swift binary target
+        .binaryTarget(
+            name: "BinarySampleHelloWorldObjC",
+            path: "Binaries/BinarySampleHelloWorldObjC.xcframework"),
+
+        // A bundled ObjC binary target
+        .binaryTarget(
+            name: "BinarySampleHelloWorldSwift",
+            path: "Binaries/BinarySampleHelloWorldSwift.xcframework")
     ]
 )
