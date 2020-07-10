592d09d8fe4935630276f952254ea30928ae6b3d
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2020-07-10 13:13:16 +0200

    create a sample app

---
+++ SampleApp/SampleApp/AccessingModules.swift
@@ -0,0 +1,8 @@
+//
+//  AccessingModules.swift
+//  SampleApp
+//
+//  Created by Rainer Schlönvoigt on 10/07/2020.
+//
+
+import Foundation
---
+++ SampleApp/SampleApp/SampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppApp.swift
+//  SampleApp
+//
+//  Created by Rainer Schlönvoigt on 10/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
