e57ae032494befb8853c99627f97985429bd2b79
Rainer Schlönvoigt <rainer.schlonvoigt@philips.com>
2020-07-10 13:45:09 +0200

    updating binaries

---
+++ Package/Binaries/BinarySampleHelloWorldObjC.xcframework/ios-x86_64-simulator/BinarySampleHelloWorldObjC.framework/Headers/BinarySampleHelloWorldObjC.h
@@ -0,0 +1,18 @@
+//
+//  BinarySampleHelloWorldObjC.h
+//  BinarySampleHelloWorldObjC
+//
+//  Created by Rainer Schlönvoigt on 10/07/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BinarySampleHelloWorldObjC.
+FOUNDATION_EXPORT double BinarySampleHelloWorldObjCVersionNumber;
+
+//! Project version string for BinarySampleHelloWorldObjC.
+FOUNDATION_EXPORT const unsigned char BinarySampleHelloWorldObjCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BinarySampleHelloWorldObjC/PublicHeader.h>
+
+#import <BinarySampleHelloWorldObjC/Echo.h>
